d5319e0896cc85b978a901951f535df613f96000
mattberman1 <mattberman1@icloud.com>
2025-06-10 14:16:45 -0700

    Resolve merge conflicts and update comments and tests

--- TripPlannerApp.swift
+++ TripPlannerApp.swift
@@ -4,39 +4,18 @@
 //
 //  Created by Matt Berman on 6/7/25.
 //
-<<<<<<< HEAD
+
+
 import SwiftUI
 
 @main
 struct TripPlannerApp: App {
-=======
-
-import SwiftUI
-import SwiftData
-
-@main
-struct TripPlannerApp: App {
-    var sharedModelContainer: ModelContainer = {
-        let schema = Schema([
-            Item.self,
-        ])
-        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
-
-        do {
-            return try ModelContainer(for: schema, configurations: [modelConfiguration])
-        } catch {
-            fatalError("Could not create ModelContainer: \(error)")
-        }
-    }()
-
->>>>>>> 7c0ee66 (Initial Commit)
+    @StateObject private var tripStore = TripStore()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(tripStore)
         }
-<<<<<<< HEAD
-=======
-        .modelContainer(sharedModelContainer)
->>>>>>> 7c0ee66 (Initial Commit)
     }
 }
