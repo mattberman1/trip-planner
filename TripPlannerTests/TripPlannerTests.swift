8a4b3206e6e62316541ccd78e42ab64b7288e52b
mattberman1 <mattberman1@icloud.com>
2025-06-16 20:57:45 -0500

    Initial Commit

---
+++ TripPlannerTests/TripPlannerTests.swift
@@ -0,0 +1,16 @@
+//
+//  TripPlannerTests.swift
+//  TripPlannerTests
+//
+//  Created by Matt Berman on 6/16/25.
+//
+
+import Testing
+
+struct TripPlannerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
